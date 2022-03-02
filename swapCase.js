a93739f9feb4ee62d39f9eaa76266d3f8ade4bca
Arianna Vargas <arvargas802@gmail.com>
2022-03-01 22:57:35 -0800

    swapCase complete.

--- swapCase.js
+++ swapCase.js
@@ -29,6 +29,31 @@ console.log(result) // "oH, hELLO!!"
 
 **/
 
-function swapCase(str) {
-	// Your code
-}
+//input: a string (input usually means a parameter)
+//output: a string with the cases swapped
+//purpose: to be able to change character cases, in order validate passwords, etc.
+//methods:
+    //a variable to contain the new string
+    //Split the string, setting the delimeter to be an empty string '', and create a variable to store the new array
+    //Create a for loop that starts at position zero, and loop through the elements of the array
+        //If true, make lowercase and add it to the vatiable with the output string
+    //Else if, test if it is lowercase
+        //If true, make uppercase and add it to the variable with the output string
+    //If false, add the character tho the string
+
+	function swapCase(str) {
+		var newString = '';
+	  var splitNew = str.split("");
+	 
+	  
+	  for(let i=0; i < splitNew.length; i++){
+	  if(splitNew[i] === splitNew[i].toUpperCase()){
+		newString = newString + splitNew[i].toLowerCase();
+	  } else {
+	   newString = newString + splitNew[i].toUpperCase();
+	 }
+	  }
+	  return newString
+	}
+	
+	console.log(swapCase("Hello World"));
