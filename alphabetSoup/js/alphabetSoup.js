cc20f6c7649fab0a39490b9659ee243ff31ab89c
Arianna Vargas <arvargas802@gmail.com>
2022-03-01 22:58:53 -0800

    alphabetSoup complete.

---
+++ alphabetSoup/js/alphabetSoup.js
@@ -0,0 +1,57 @@
+/* 
+Write a function alphabetSoup(str) that takes a string and returns a string with
+the letters in alphabetical order. Assume the input will not contain numbers or punctuation.
+Hint: Check out split(), sort() and join() methods at 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+
+
+Bonus:
+If there are spaces in the input, do not include them in the result.
+Hint: Check out trim() method at
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+*/
+
+/**
+Examples:
+
+var result = alphabetSoup("hello");
+console.log(result) // "ehllo"
+
+var result = alphabetSoup("");
+console.log(result) // ""
+
+var result = alphabetSoup("f");
+console.log(result) // "f"
+
+var result = alphabetSoup("my string");
+console.log(result) // " gimnrsty"
+
+Bonus:
+var result = alphabetSoup("my string oh my");
+console.log(result) // "ghimmnorstyy"
+
+**/
+
+function alphabetSoup(str) {
+	var stringToSort = str ;
+  var splitString = stringToSort.split("");
+  var sortString = splitString.sort();
+  var joinedArray = sortString.join(" ");
+	console.log(joinedArray);	
+}
+
+alphabetSoup("hello ari");
+
+
+function alphabetChained(str) {
+ 
+    var answer = str.split("").sort().join("");
+  console.log(answer)
+}
+  alphabetChained("great work everyone");
+
+  
+
